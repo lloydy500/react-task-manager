1437532412f63df4d60efea0fa1a1e4a6767626c
Lloyd Chapple <lloydchaps@gmail.com>
2021-05-10 11:38:19 +0100

    add/remove tasks

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,20 +1,19 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
+    
         return (
             <header className='header'>
             <h1 >{title}</h1>
             <Button
-            color='green'
-            text='Add'
-            onClick={onClick}
+            color={showAdd ? 'red' : 'green' }
+            text={showAdd ? 'Close' : 'Add' }
+            onClick={onAdd}
             />
         </header>
-        )}
+        )    
+    }
         
 Header.defaultProps = {
     title: 'Task Tracker',
